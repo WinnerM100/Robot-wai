8b5466f542d020da8723dc843e3a20863ae5773a
vladalexan <51922137+vladalexan@users.noreply.github.com>
2020-08-16 10:45:55 +0300

    movement basic facut
    mai are nevoie de un recharge pt dash

---
+++ Robot/Assets/Scripts/MobileInput.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : MonoBehaviour
+{
+    public const float DEADZONE = 75.0f;
+    public static MobileInput Instance { set; get; }
+
+    public bool tap, swipeUp, swipeDown, swipeLeft, swipeRight;
+    public Vector2 swipeDelta, startTouch;
+    public bool Tap { get { return tap; } }
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Update()
+    {
+        //reset
+        tap = swipeUp = swipeDown = swipeLeft = swipeRight = false;
+
+        //input
+
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            startTouch = swipeDelta = Vector2.zero;
+        }
+        #endregion
+
+        #region Mobile Input
+        if (Input.touches.Length != 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                startTouch = Input.mousePosition;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                startTouch = swipeDelta = Vector2.zero;
+            }
+
+        }
+
+        #endregion
+
+        //Calculate Distance
+        swipeDelta = Vector2.zero;
+        if (startTouch != Vector2.zero)
+        {
+            //check with mobile
+            if (Input.touches.Length != 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+
+            //check with standalone
+            else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+        //calculate deadzone
+        if (swipeDelta.magnitude >= DEADZONE)
+        {
+            //swipe confirmed
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Math.Abs(x) < Math.Abs(y))
+            {
+                if (y < 0)
+                {
+                    swipeDown = true;
+                }
+                else
+                {
+                    swipeUp = true;
+                }
+            }
+            if (Math.Abs(x) >= Math.Abs(y))
+            {
+                if (x < 0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+
+            startTouch = swipeDelta = Vector2.zero;
+        }
+    }
+}
---
+++ Robot/Assets/Scripts/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    //public CharacterController MobileInput;
+    public float speed;
+    public float dashSpeed;
+    public float dashSpeed2;
+    public GameObject robot;
+    public int dashes = 2;
+    public float LANE_DISTANCE;
+    public Vector3 targetPos;
+
+    private Rigidbody2D robotBody;
+
+    private float screenWidth;
+
+    void Start()
+    {
+        screenWidth = Screen.width; //here just in case
+        robotBody = robot.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > (float)15/16 * screenWidth) //dreapta
+            {
+                move(1.0f);
+            }
+            if (Input.GetTouch(i).position.x < screenWidth / 16) //stanga
+            {
+                move(-1.0f);
+            }
+            ++i;
+        }
+        //if (Time.timeScale >= 1f)
+        //{
+        if (MobileInput.Instance.SwipeUp && dashes>0)
+        {
+            targetPos = new Vector3(transform.position.x, transform.position.y + LANE_DISTANCE, transform.position.z);
+            dashes--;
+            Vector3 direction = targetPos - transform.position;
+            robotBody.AddForce(transform.position + direction *  dashSpeed2 * Time.deltaTime);
+        }
+        if (MobileInput.Instance.SwipeDown && dashes>0)
+        {
+            float pound = 3.0f * dashSpeed2;
+            targetPos = new Vector3(transform.position.x, transform.position.y - LANE_DISTANCE, transform.position.z);
+            dashes--;
+            Vector3 direction = targetPos - transform.position;
+            robotBody.AddForce(transform.position + direction * pound * Time.deltaTime);
+        }
+        if (MobileInput.Instance.SwipeLeft && dashes > 0)
+        {
+            targetPos = new Vector3(transform.position.x - LANE_DISTANCE, transform.position.y, transform.position.z);
+            dashes--;
+            Vector3 direction = targetPos - transform.position;
+            robotBody.MovePosition(transform.position + direction * dashSpeed * Time.deltaTime);
+        }
+        if (MobileInput.Instance.SwipeRight && dashes > 0)
+        {
+            targetPos = new Vector3(transform.position.x + LANE_DISTANCE, transform.position.y, transform.position.z);
+            dashes--;
+            Vector3 direction = targetPos - transform.position;
+            robotBody.MovePosition(transform.position + direction * dashSpeed * Time.deltaTime);
+        }
+    }
+    
+    //functie pt editor
+    private void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        move(Input.GetAxis("Horizontal"));
+#endif
+    }
+    
+    private void move(float horizontalInput) //butoane
+    {
+        robotBody.AddForce(new Vector2(horizontalInput * speed * Time.deltaTime, 0)); //functie pt miscare
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
